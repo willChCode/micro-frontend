d182aff8391c8f1277ff9a07c407257fdc679035
willCh <willchcode@gmail.com>
2023-10-27 14:34:32 -0400

    first commit

---
+++ mf-login/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-lg text-red-500">LOGIN</h1>
+    <InputC />
+  </div>
+</template>
+
+<script setup>
+import InputC from './components/InputC.vue';
+</script>
+
+<style lang="scss" scoped></style>
---
+++ mf-login/src/components/InputC.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <input type="text" />
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
